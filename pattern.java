fc0b83f91338223a060570b16b242dc42a15f7d6
Priyanshu <bothulk1620@gmail.com>
2024-04-17 03:18:46 +0530

    2 basic Pattern problems

---
+++ pattern.java
@@ -0,0 +1,41 @@
+public class pattern {
+    public static void main(String args[]){
+        
+    /* // # 1st - Pattern(square) :
+        // * * * * * 
+        // * * * * *
+        // * * * * *
+        // * * * * *
+        // * * * * *
+
+        int n = 5; 
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        } */
+
+
+    /* //# 2nd - Pattern(Hollow Square) :
+        //   * * * * * 
+        //   *       * 
+        //   *       * 
+        //   *       * 
+        //   * * * * * 
+
+        // int n = 4;
+        // for(int i = 0; i<=n; i++) {
+        //     for(int j = 0; j<=n; j++){
+        //         if(i == 0 || j == 0 || i == n  || j == n){
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // } */
+
+        
+    }
+}
