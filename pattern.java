fccf0564dc00dc71b631b8f322b78930ec34ca84
Priyanshu <bothulk1620@gmail.com>
2024-04-18 00:07:33 +0530

    new Pattern

--- pattern.java
+++ pattern.java
@@ -72,16 +72,16 @@ public class pattern {
 
     //# 5th - Pattern(Left Half Pyramid) :
 
-    int n = 5;
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j < i; j++) {
-            System.out.print(" ");
+    
+        for(int i = 0; i<5-1; i++){
+            for(int j = 5-1; j>i; j-- ){
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-        for (int j = 0; j <= n - i; j++) {
-            System.out.print("* ");
-        }
-        System.out.println();
-    }
         
     }
 }
