3c9a1c5aa748592b1b854e28f45a84d58cab6d72
Priyanshu <bothulk1620@gmail.com>
2024-04-17 21:57:59 +0530

    new pattern

--- pattern.java
+++ pattern.java
@@ -36,6 +36,52 @@ public class pattern {
         //     System.out.println();
         // } */
 
+
+    /* //# 3rd - Pattern(Right Half Pyramid) :
+        //   * 
+        //   * * 
+        //   * * * 
+        //   * * * * 
+        //   * * * * * 
+        //   * * * * * *
+
+        // int n = 5;
+        // for(int i = 0; i<=n; i++){
+        //     for(int j = 0; j<=i; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // } */
+
+
+    /* //# 4th - Pattern(Reverse Right Half Pyramid) :
+       // * * * * * 
+       // * * * * 
+       // * * * 
+       // * * 
+       // * 
+
+    int n = 5;
+    for(int i = 0; i<n; i++){
+        for(int j = n; j>i; j--){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }  */
+
+
+    //# 5th - Pattern(Left Half Pyramid) :
+
+    int n = 5;
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j < i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 0; j <= n - i; j++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
         
     }
 }
